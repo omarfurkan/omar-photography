b03ca34fa33aed4c58ef04f7b8e5e3fecdcbfdae
omarfurkan <omarfurkan92@gmail.com>
2022-08-14 15:07:40 +0600

    hide firebase key

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -12,7 +12,9 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID
+
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
